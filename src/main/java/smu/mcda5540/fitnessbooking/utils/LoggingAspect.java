d0e40dd6be5634685816db8390df3b030a46c844
Nghia <19204686+nghiamphan@users.noreply.github.com>
2023-11-26 17:54:22 -0400

    print request and response messages

--- src/main/java/smu/mcda5540/fitnessbooking/utils/LoggingAspect.java
+++ src/main/java/smu/mcda5540/fitnessbooking/utils/LoggingAspect.java
@@ -2,17 +2,35 @@ package smu.mcda5540.fitnessbooking.utils;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class LoggingAspect {
     private static final Log LOGGER = LogFactory.getLog(LoggingAspect.class);
+    private final Environment environment;
+
+    @Autowired
+    public LoggingAspect(Environment environment) {
+        this.environment = environment;
+    }
 
     @AfterThrowing(pointcut = "execution(* smu.mcda5540.fitnessbooking.*.*Impl.*(..))", throwing = "e")
     public void afterThrowing(Exception e) {
-        LOGGER.error(e.getMessage());
+        if (e instanceof FitnessBookingException) {
+            LOGGER.error(environment.getProperty(e.getMessage()));
+        } else {
+            LOGGER.error(e.getMessage());
+        }
+    }
+
+    @AfterReturning(pointcut = "execution(* smu.mcda5540.fitnessbooking.controller.*.*(..))", returning = "response")
+    public void logResponse(Object response) {
+        LOGGER.info("RESPONSE: " + response);
     }
 }
---
+++ src/main/java/smu/mcda5540/fitnessbooking/utils/RequestLoggingFilterConfig.java
@@ -0,0 +1,19 @@
+package smu.mcda5540.fitnessbooking.utils;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingFilterConfig {
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter
+                = new CommonsRequestLoggingFilter();
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(10000);
+        filter.setIncludeHeaders(false);
+        filter.setAfterMessagePrefix("REQUEST: ");
+        return filter;
+    }
+}
