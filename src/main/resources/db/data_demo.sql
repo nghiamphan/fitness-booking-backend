afd69b37727d001e2827ffb95a668d3ac645d0f7
Nghia <19204686+nghiamphan@users.noreply.github.com>
2023-12-13 22:48:37 -0400

    demo data

--- src/main/resources/db/data_demo.sql
+++ src/main/resources/db/data_demo.sql
@@ -1,118 +1,117 @@
-DROP TABLE IF EXISTS person, instructor, program, qualification, location, class, register;
+DROP TABLE IF EXISTS person, instructor, program, qualification, location, class, booking;
 
 CREATE TABLE person
 (
-    _id        INT         NOT NULL AUTO_INCREMENT,
+    person_id  INT         NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name  VARCHAR(30) NOT NULL,
-    email      VARCHAR(50) NOT NULL,
-    PRIMARY KEY (_id)
+    email      VARCHAR(50) NOT NULL UNIQUE,
+    username   VARCHAR(30) NOT NULL UNIQUE,
+    password   VARCHAR(30) NOT NULL,
+    PRIMARY KEY (person_id)
 );
 
 CREATE TABLE instructor
 (
-    _id            INT           NOT NULL AUTO_INCREMENT,
-    person_id      INT           NOT NULL,
+    instructor_id  INT           NOT NULL,
     bio            VARCHAR(1000) NOT NULL,
     business_phone VARCHAR(20)   NOT NULL,
-    PRIMARY KEY (_id),
-    FOREIGN KEY (person_id)
-        REFERENCES person (_id)
+    PRIMARY KEY (instructor_id),
+    FOREIGN KEY (instructor_id)
+        REFERENCES person (person_id)
 );
 
 CREATE TABLE program
 (
-    _id         INT          NOT NULL AUTO_INCREMENT,
+    program_id  INT          NOT NULL AUTO_INCREMENT,
     name        VARCHAR(30)  NOT NULL,
     description VARCHAR(500) NOT NULL,
-    PRIMARY KEY (_id)
+    PRIMARY KEY (program_id)
 );
 
 CREATE TABLE qualification
 (
-    _id           INT NOT NULL AUTO_INCREMENT,
     instructor_id INT NOT NULL,
     program_id    INT NOT NULL,
-    PRIMARY KEY (_id),
+    PRIMARY KEY (instructor_id, program_id),
     FOREIGN KEY (instructor_id)
-        REFERENCES instructor (_id),
+        REFERENCES instructor (instructor_id),
     FOREIGN KEY (program_id)
-        REFERENCES program (_id),
+        REFERENCES program (program_id),
     UNIQUE KEY unique_key (instructor_id, program_id)
 );
 
 CREATE TABLE location
 (
-    _id         INT          NOT NULL AUTO_INCREMENT,
+    location_id INT          NOT NULL AUTO_INCREMENT,
     name        VARCHAR(30)  NOT NULL,
     description VARCHAR(200) NOT NULL,
-    area        INT          NOT NULL,
-    PRIMARY KEY (_id)
+    room_size   INT          NOT NULL,
+    PRIMARY KEY (location_id)
 );
 
 CREATE TABLE class
 (
-    _id             INT      NOT NULL AUTO_INCREMENT,
-    start_time      DATETIME NOT NULL,
-    end_time        DATETIME NOT NULL,
-    available_space INT,
-    instructor_id   INT      NOT NULL,
-    program_id      INT      NOT NULL,
-    location_id     INT      NOT NULL,
-    PRIMARY KEY (_id),
+    class_id       INT      NOT NULL AUTO_INCREMENT,
+    start_time     DATETIME NOT NULL,
+    end_time       DATETIME NOT NULL,
+    total_capacity INT,
+    instructor_id  INT      NOT NULL,
+    program_id     INT      NOT NULL,
+    location_id    INT      NOT NULL,
+    PRIMARY KEY (class_id),
     FOREIGN KEY (instructor_id)
-        REFERENCES instructor (_id),
+        REFERENCES instructor (instructor_id),
     FOREIGN KEY (program_id)
-        REFERENCES program (_id),
+        REFERENCES program (program_id),
     FOREIGN KEY (location_id)
-        REFERENCES location (_id)
+        REFERENCES location (location_id)
 );
 
-CREATE TABLE register
+CREATE TABLE booking
 (
-    _id       INT NOT NULL AUTO_INCREMENT,
     person_id INT NOT NULL,
     class_id  INT NOT NULL,
-    PRIMARY KEY (_id),
+    PRIMARY KEY (person_id, class_id),
     FOREIGN KEY (person_id)
-        REFERENCES person (_id),
+        REFERENCES person (person_id),
     FOREIGN KEY (class_id)
-        REFERENCES class (_id),
+        REFERENCES class (class_id),
     UNIQUE KEY unique_key (person_id, class_id)
 );
 
 
-insert into person (first_name, last_name, email)
-values ('First name 1', 'Last name 1', 'email1'),
-       ('First name 2', 'Last name 2', 'email2'),
-       ('First name 3', 'Last name 3', 'email3'),
-       ('First name 4', 'Last name 4', 'email4'),
-       ('First name 5', 'Last name 5', 'email5'),
-       ('First name 6', 'Last name 6', 'email6'),
-       ('First name 7', 'Last name 7', 'email7'),
-       ('First name 8', 'Last name 8', 'email8'),
-       ('First name 9', 'Last name 9', 'email9'),
-       ('First name 10', 'Last name 10', 'email10'),
-       ('First name 11', 'Last name 11', 'email11'),
-       ('First name 12', 'Last name 12', 'email12'),
-       ('First name 13', 'Last name 13', 'email13'),
-       ('First name 14', 'Last name 14', 'email14'),
-       ('First name 15', 'Last name 15', 'email15'),
-       ('First name 16', 'Last name 16', 'email16'),
-       ('First name 17', 'Last name 17', 'email17'),
-       ('First name 18', 'Last name 18', 'email18'),
-       ('First name 19', 'Last name 19', 'email19');
-insert into instructor (person_id, bio, business_phone)
-values (1, 'Bio 1', 'Phone 1'),
-       (2, 'Bio 2', 'Phone 2'),
-       (3, 'Bio 3', 'Phone 3'),
-       (4, 'Bio 4', 'Phone 4'),
-       (5, 'Bio 5', 'Phone 5');
+insert into person (first_name, last_name, email, username, password)
+values ('First name 1', 'Last name 1', 'email1@gmail.com', 'user1', 'pass'),
+       ('First name 2', 'Last name 2', 'email2@gmail.com', 'user2', 'pass'),
+       ('First name 3', 'Last name 3', 'email3@gmail.com', 'user3', 'pass'),
+       ('First name 4', 'Last name 4', 'email4@gmail.com', 'user4', 'pass'),
+       ('First name 5', 'Last name 5', 'email5@gmail.com', 'user5', 'pass'),
+       ('First name 6', 'Last name 6', 'email6@gmail.com', 'user6', 'pass'),
+       ('First name 7', 'Last name 7', 'email7@gmail.com', 'user7', 'pass'),
+       ('First name 8', 'Last name 8', 'email8@gmail.com', 'user8', 'pass'),
+       ('First name 9', 'Last name 9', 'email9@gmail.com', 'user9', 'pass'),
+       ('First name 10', 'Last name 10', 'email10@gmail.com', 'user10', 'pass'),
+       ('First name 11', 'Last name 11', 'email11@gmail.com', 'user11', 'pass'),
+       ('First name 12', 'Last name 12', 'email12@gmail.com', 'user12', 'pass'),
+       ('First name 13', 'Last name 13', 'email13@gmail.com', 'user13', 'pass'),
+       ('First name 14', 'Last name 14', 'email14@gmail.com', 'user14', 'pass'),
+       ('First name 15', 'Last name 15', 'email15@gmail.com', 'user15', 'pass'),
+       ('First name 16', 'Last name 16', 'email16@gmail.com', 'user16', 'pass'),
+       ('First name 17', 'Last name 17', 'email17@gmail.com', 'user17', 'pass'),
+       ('First name 18', 'Last name 18', 'email18@gmail.com', 'user18', 'pass'),
+       ('First name 19', 'Last name 19', 'email19@gmail.com', 'user19', 'pass');
+insert into instructor (instructor_id, bio, business_phone)
+values (1, 'Bio 1', '902-123-4567'),
+       (2, 'Bio 2', '902-123-5678'),
+       (3, 'Bio 3', '902-123-6789'),
+       (4, 'Bio 4', '902-345-4567'),
+       (5, 'Bio 5', '902-678-4567');
 insert into program (name, description)
 values ('Core Strength',
         'This class focuses on core conditioning, stabilization, and strength through a variety of exercises. Be prepared to work on proper movement, challenge your core and feel the burn through core targeting compound movements and mat exercises.'),
        ('Meditation',
-        'Calm your mind and body and address your stressors both positive and negative with this relaxing Yoga Nidra style meditation. This meditation practice will increase delta brainwaves and access your inner calm, and creativity more freely. Other benefits can include reduced anxiety, chronic pain, depression, and blood pressure.'),
+        'Calm your mind and body and address your stressors both positive and negative with this relaxing Yoga Nidra style meditation. This meditation practice will increase delta brainwaves and access your inner calm, and creativity more freely.'),
        ('Spin',
         'Get on your bikes and ride! This class uses Keiser spin bikes to give you the best low impact cardio workout around. Burn calories and have a blast doing it throughout this interval style spin workout. Enjoy hills and valley\'s all from the comfort of our studio.'),
        ('Synergy Circuit',
@@ -138,13 +137,13 @@ values (1, 1),
        (5, 5),
        (5, 6),
        (5, 7);
-insert into location (name, description, area)
+insert into location (name, description, room_size)
 values ('Studio A', 'Great for fitness, yoga, dance or workout space', 988),
        ('Studio B', 'Floating hardwood floor where pilates, yoga, spin, and a variety of fitness classes are offered.',
         1022),
        ('Studio C', 'Floating hardwood floor where pilates, yoga, spin, and a variety of fitness classes are offered.',
         1730);
-insert into class (start_time, end_time, available_space, instructor_id, program_id, location_id)
+insert into class (start_time, end_time, total_capacity, instructor_id, program_id, location_id)
 values ('2023-12-16 10:00:00', '2023-12-16 10:45:00', 18, 1, 1, 1),
        ('2023-12-17 12:00:00', '2023-12-17 12:45:00', 18, 1, 2, 1),
        ('2023-12-18 10:00:00', '2023-12-18 10:45:00', 18, 1, 3, 2),
@@ -159,7 +158,7 @@ values ('2023-12-16 10:00:00', '2023-12-16 10:45:00', 18, 1, 1, 1),
        ('2023-12-19 11:00:00', '2023-12-19 11:45:00', 18, 5, 5, 2),
        ('2023-12-20 9:00:00', '2023-12-20 9:45:00', 18, 5, 6, 3),
        ('2023-12-21 9:00:00', '2023-12-21 9:45:00', 18, 5, 7, 1);
-insert into register (person_id, class_id)
+insert into booking (person_id, class_id)
 values (6, 1),
        (6, 2),
        (6, 3),
